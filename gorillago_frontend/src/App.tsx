843361424ad5f0e42d828a1e8dfcc2de9f6e13ed
Dani <hostnameadmin@protonmail.com>
2023-02-08 11:45:29 +0100

    register working, error messages missing

--- gorillago_frontend/src/App.tsx
+++ gorillago_frontend/src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+interface State {
+
+  email:string;
+  password: string;
+  rePassword: string;
+}
+
+class App extends Component<{}, State>{
+
+  constructor(props: {}){
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      rePassword: '',
+      
+    }
+  }
+
+
+  handleRegister = async () => {
+    const { email, password, rePassword } = this.state;
+    if(email.trim() === '' || password!== rePassword){
+      // this.setState()- tel hibaüzenet megjelenítése
+      return;
+    }
+
+    const adat = {
+      email: email,
+      password: password,
+      rePassword: rePassword
+    }
+
+    let response = await fetch('http://localhost:3000/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(adat),
+    });
+
+    this.setState({ 
+      email: '',
+      password: '',
+      rePassword: '',
+    })
+
+    console.log(adat);
+
+  };
+
+  render(){
+    return <div className='Inputs'>
+      
+      <h2>Regisztráció</h2>
+      Email: <input type="text"  value={this.state.email} onChange={e => this.setState({ email: e.currentTarget.value})} /> <br />
+      Jelszó: <input type="password"  value={this.state.password} onChange={e => this.setState({ password: e.currentTarget.value})} /> <br />
+      Jelszó ismét: <input type="password"  value={this.state.rePassword} onChange={e => this.setState({ rePassword: e.currentTarget.value})} /> <br />
+      <button onClick={this.handleRegister}>Regisztráció</button>
+     
+      
     </div>
-  );
+  }
 }
 
 export default App;
